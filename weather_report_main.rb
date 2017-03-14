9df2dfbbecab3e98782f727809dc81f7cce6d8b8
britth <brittmhayes@gmail.com>
2017-03-14 16:34:41 -0400

    Add some handling for invalid zipcodes

--- weather_report_main.rb
+++ weather_report_main.rb
@@ -1,8 +1,27 @@
 require './weather_report.rb'
 
-puts 'Enter a five-digit zip code to get a report of current weather conditions in that area'
+puts 'Enter a five-digit zip code to get a report of current weather conditions in that area. Enter "0" if you want to quit'
 zip = gets.chomp
-puts "Entered #{zip}"
 
-report = WeatherReport.find(zip)
-puts report.narrative
+response = WeatherReport.find(zip)
+attempts = 1
+
+while (attempts <= 3 and zip != "0") do
+  if zip == "0"
+    "Goodbye!"
+    break
+  elsif not response.error.nil? and attempts < 3
+    puts "Sorry: #{response.error}."
+    puts "Enter another five-digit zip code. Or, press '0' to exit"
+    zip = gets.chomp
+    response = WeatherReport.find(zip)
+    attempts += 1
+    #puts response.error
+  elsif not response.error.nil? and attempts == 3
+    puts "Sorry: #{response.error}. Try again in a few minutes."
+    attempts += 1
+  else
+    puts response.narrative
+    break
+  end
+end
